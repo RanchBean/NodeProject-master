3fc8cc4209e8ebc8c41ab1efe36a221a88097261
Ethan throndsen <Ethunderson1@gmail.com>
2016-01-29 12:21:40 -0700

    added a header file and cpp

---
+++ src/Model/ArrayNode.cpp
@@ -0,0 +1,18 @@
+/*
+ * ArrayNode.cpp
+ *
+ *  Created on: Jan 29, 2016
+ *      Author: ethr5627
+ */
+
+#include "ArrayNode.h"
+
+ArrayNode::ArrayNode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ArrayNode::~ArrayNode() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Model/ArrayNode.h
@@ -0,0 +1,25 @@
+/*
+ * ArrayNode.h
+ *
+ *  Created on: Jan 29, 2016
+ *      Author: ethr5627
+ */
+
+#ifndef MODEL_ARRAYNODE_H_
+#define MODEL_ARRAYNODE_H_
+
+#include "Node.h"
+template <class Type>
+
+class ArrayNode: public Node<Type>
+{
+private :
+	ArrayNode * next;
+public:
+	ArrayNode();
+	ArrayNode(Type value);
+	ArrayNode(Type valuse, ArrayNode * next);
+	virtual ~ArrayNode();
+};
+
+#endif /* MODEL_ARRAYNODE_H_ */
